9770f64bed9c3c29f6a617e1906bcf84a4520a43
Xiao G. Wu <xiaogwu@gmail.com>
2013-07-16 13:51:29 -0700

    Add Home page

--- spec/features/static_pages_spec.rb
+++ spec/features/static_pages_spec.rb
@@ -1,7 +1,24 @@
 require 'spec_helper'
 
 describe "Static Pages" do
+  
+  describe "Home page" do
+    it "should have the right heading" do
+      visit '/static_pages/home'
+      page.should have_selector('h1', :text => "Home Page")
+    end
+    it "should have the content 'Home Page'" do
+      visit '/static_pages/home'
+      page.should have_content('Home Page')
+    end
+    
+  end
+
   describe "Admin page" do
+    it "should have the right heading" do
+      visit '/static_pages/admin'
+      page.should have_selector('h1', :text => "Admin Page")
+    end
     it "should have the content 'Admin Page'" do
       visit '/static_pages/admin'
       page.should have_content('Admin Page')
