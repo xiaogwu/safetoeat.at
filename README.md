f8a3a65e09c7ea1c3180905c61e42475aee8dae2
Xiao G. Wu <xiaogwu@gmail.com>
2013-07-16 00:03:13 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# GA WDI Project #1
+## safetoeat.at
+Rails app to search and display SF Health Inspection Scores
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SafetoeatAt::Application.config.secret_token = '4952b6f546cd03411986a2316b8aa639cadf165908a6fda630fd71379f57aeb7ecaae35e157751a6b1d960f3d54ed451cb6ff76339a16eaf382e97ad0f967a00'
